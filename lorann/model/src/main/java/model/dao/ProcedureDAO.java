e6adc272d888a6a341204b874417a450a80c4ec7
guill <guill@MSI>
2018-05-30 10:18:37 +0200

    call of procedure (remake)

--- lorann/model/src/main/java/model/dao/ProcedureDAO.java
+++ lorann/model/src/main/java/model/dao/ProcedureDAO.java
@@ -1,5 +1,84 @@
 package model.dao;
 
-public abstract class ProcedureDAO {
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Example;
+/**
+ * <h1>The Class ProcedureDAO.</h1>
+ *
+ * @author Guillaume PIRES guillaume.pires@cesi.fr
+ * @version 1.0
+ */
+public abstract class ProcedureDAO extends AbstractDAO{
+
+
+	    /** The sql map by level. */
+	    private static String sqlMapByLevel   = "{call findMapByLevel(?)}";
+
+	    /** The sql example by name. 
+	    private static String sqlExampleByName = "{call findExampleByName(?)}";
+*/
+	    /** The sql all examples. 
+	    private static String sqlAllExamples   = "{call findAllExamples()}";
+*/
+	    /** The Element column index. */
+
+	    private static int    ElementIndex    = 2;
+
+	    /** The X column index. */
+	    private static int    XPositionIndex    = 3;
+
+	    /** The Y column index. */
+	    private static int    YPositionIndex  = 4;
+
+	    /**
+	     * Gets the example by id.
+	     *
+	     * @param id
+	     *            the id
+	     * @return the example by id
+	     * @throws SQLException
+	     *             the SQL exception
+	     */
+	    public static Example getMapByLevel(final int Level) throws SQLException {
+	        final CallableStatement callStatement = prepareCall(sqlMapByLevel);
+	        Example procedure = null;
+	        callStatement.setInt(1, Level);
+	        if (callStatement.execute()) {
+	            final ResultSet result = callStatement.getResultSet();
+	            if (result.first()) {
+	                procedure = new Example(result.getString(ElementIndex),result.getInt(XPositionIndex), result.getInt(YPositionIndex));
+	            }
+	            result.close();
+	        }
+	        return procedure;
+	    }
+
+	    /**
+	     * Gets the example by name.
+	     *
+	     * @param name
+	     *            the name
+	     * @return the example by name
+	     * @throws SQLException
+	     *             the SQL exception
+	     */
+	  /*  public static Example getExampleByName(final String name) throws SQLException {
+	        final CallableStatement callStatement = prepareCall(sqlExampleByName);
+	        Example example = null;
+
+	        callStatement.setString(1, name);
+	        if (callStatement.execute()) {
+	            final ResultSet result = callStatement.getResultSet();
+	            if (result.first()) {
+	                example = new Example(result.getInt(idColumnIndex), result.getString(nameColumnIndex));
+	            }
+	            result.close();
+	        }
+	        return example;
+	    }*/
 }

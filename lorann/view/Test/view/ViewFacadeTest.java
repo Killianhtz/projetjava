2f0d2d464e079942433e4cd64490caab65030497
Killianhtz <32952424+Killianhtz@users.noreply.github.com>
2018-06-04 21:33:13 +0200

    creation of test classes for controller and view

---
+++ lorann/view/Test/view/ViewFacadeTest.java
@@ -0,0 +1,69 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ViewFacadeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testViewFacade() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateJFrame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDirection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDirection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMap() {
+		fail("Not yet implemented");
+	}
+
+}

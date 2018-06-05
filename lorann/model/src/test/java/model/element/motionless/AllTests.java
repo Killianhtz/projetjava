d3ca2fdf8dac9a11acceccaa75280b56cc69faf0
Killianhtz <32952424+Killianhtz@users.noreply.github.com>
2018-06-05 20:21:26 +0200

    Create Alltests classes

---
+++ lorann/model/src/test/java/model/element/motionless/AllTests.java
@@ -0,0 +1,11 @@
+package model.element.motionless;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BlockingMotionlessFactoryTest.class })
+public class AllTests {
+
+}
---
+++ lorann/view/src/test/java/view/AllTests.java
@@ -0,0 +1,11 @@
+package view;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ViewFacadeTest.class, ViewJFrameTest.class, ViewJPanelTest.class })
+public class AllTests {
+
+}

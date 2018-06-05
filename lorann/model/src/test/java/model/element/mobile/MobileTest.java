80677c51710f95cd270a26f6568fb3ffb748ada8
atnprd <antoineperrenoud02@gmail.com>
2018-06-05 23:04:31 +0200

    Remove unused imports in Mobile test

--- lorann/model/src/test/java/model/element/mobile/MobileTest.java
+++ lorann/model/src/test/java/model/element/mobile/MobileTest.java
@@ -3,9 +3,7 @@ package model.element.mobile;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MobileTest {
